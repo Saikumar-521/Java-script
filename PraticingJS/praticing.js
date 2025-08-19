9c97ecb8fe91d4e2a49b7954158c168622b78d75
gadaboinasaikumar521 <gadaboinasaikumar521@gmail.com>
2025-08-19 19:48:47 +0530

    Pratice JS

--- PraticingJS/praticing.js
+++ PraticingJS/praticing.js
@@ -4,3 +4,19 @@ console.log(!"");//""--->empty string consider as 0 false and here not operator 
 console.log(!" ");//
 console.log("sai">"SAI");
 console.log("a">"p");
+
+for(var i=0;i<5;i++){
+    console.log(i);
+}
+
+for (var i = 0; i < 5; i++) {
+  setTimeout(() => console.log(i), 5000);
+}
+for (let i = 0; i < 5; i++);
+{
+  console.log("Hello");
+}
+
+setTimeout(()=> {
+    console.log("Hello After 5 seconds");
+},5000);
