f29ac7eb57956f1c12ff52d530081a36795e9405
gadaboinasaikumar521 <gadaboinasaikumar521@gmail.com>
2025-08-20 21:53:39 +0530

    jS Pratice

--- PraticingJS/praticing.js
+++ PraticingJS/praticing.js
@@ -1,4 +1,6 @@
 /*
+coversion--->emplict conversion
+typeconversion---->implict conversion
  */
 console.log(!"");//""--->empty string consider as 0 false and here not operator convert into true and output is true
 console.log(!" ");//
@@ -20,3 +22,38 @@ for (let i = 0; i < 5; i++);
 setTimeout(()=> {
     console.log("Hello After 5 seconds");
 },5000);
+function function2(){
+ //var name="sai";
+ let name="sai";
+ console.log(name);
+}
+function2();
+//console.log(name); //througs not defined
+
+
+{
+  var blocks=20;
+  console.log(blocks);
+}
+console.log(blocks);//when the variable type is let not give access
+
+let paymentmode="Cash";
+switch(paymentmode){
+  case "Cash":
+    console.log("payment Process via Cash");
+    break;
+  case "Paytem":
+    console.log("payment process via Paytem");
+    break;
+  case "Credit card":
+    console.log("payment Process  via credit card ");
+    break;
+  case "Debit card":
+    console.log("paymrnt Process via Debit card");
+    break;
+  case "Net Banking":
+    console.log("payment Process  via Net banking");
+    break;
+  default:
+    console.log("select payment process");
+}
