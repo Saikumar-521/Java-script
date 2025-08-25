42755bab3f75b573e82adb12a64eebb5d654250a
gadaboinasaikumar521 <gadaboinasaikumar521@gmail.com>
2025-08-25 11:30:37 +0530

    pratice

--- notes&praticingofJS/praatice.js
+++ notes&praticingofJS/praatice.js
@@ -3,3 +3,11 @@ console.log(typeof undefined + "❣️  ❣️❣️❣️");
 console.log("hi", "sai kumar" + `sai kumar ${"sai kumar"}`);
 //console.log(window);
 console.log(" hi, \n iam sai kumar yadavs \n from SIDDIPET,");
+
+const sai = [1, "sai", 521, 50];
+for (let i = 0; i < sai.length; i++) {
+    console.log(i, sai[i]);
+    if (i == 1)
+        console.log(sai.push(" kumar"));
+}
+console.log(sai);
