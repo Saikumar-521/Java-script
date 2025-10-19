6c68c247c3a5bffe0ab14732380d4c245859033b
gadaboinasaikumar521 <gadaboinasaikumar521@gmail.com>
2025-10-19 12:59:19 +0530

    Binding

--- Binding/advBinding.js
+++ Binding/advBinding.js
@@ -1,4 +1,12 @@
 /*
 what is binding:
-binding is the process
+binding is the process of setting the value of 'this' keyword in a function to a specific object or context.
+There are different types of binding in JavaScript:
+1. Implicit Binding
+2. Explicit Binding
+3. New Binding
+4. Default Binding
+5. Advanced Binding
+Each type of binding determines how the 'this' keyword behaves in different scenarios when a function is invoked.
+
 */
